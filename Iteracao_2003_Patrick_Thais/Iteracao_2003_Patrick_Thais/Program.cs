3b9c91fa190504ed33ea89328d3fa01268b0803a
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2016-08-12 08:55:01 -0300

    Add files via upload

---
+++ Iteracao_2003_Patrick_Thais/Iteracao_2003_Patrick_Thais/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patrick_Thais_Iteracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("resultado");
+            Console.ReadLine();
+            int[] val = new int[5] { 1, 2, 3, 4, 5 };
+            int resultado = Somar(val);
+
+        }
+        static int Somar(int[] valores)
+        {
+            int resultado = 0;
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                resultado += valores[i];
+            }
+            return resultado;
+
+        }
+
+    }
+}
---
+++ Iteracao_2003_Patrick_Thais/Iteracao_2003_Patrick_Thais/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Iteracao_2003_Patrick_Thais")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Iteracao_2003_Patrick_Thais")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2935932-7921-419f-a70e-b27b8038480d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
